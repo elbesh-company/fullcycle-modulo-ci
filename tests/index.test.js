238c6eaedb9de56905e4ca67837a7ae9a8c7db47
Elbes Henrique <elbesh@gmail.com>
2021-08-06 07:57:10 -0300

    Refactor sum test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,6 @@
 const { sum } = require('../src/index');
 
-test('1 should be 1', () => {
+test('When summing 3 and 3 result should be 6', () => {
     const actual = sum(3, 3);
     const expected = 6;
     expect(actual).toBe(expected);
